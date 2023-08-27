696367b6284b2a8279555063b87e2351ac04f789
choewy <choewy32@gmail.com>
2023-08-27 22:07:23 +0900

    feature/leetcode 02. add two numbers

---
+++ typescript/solved/leetcode-2-add-two-numbers.ts
@@ -0,0 +1,58 @@
+/** @description https://leetcode.com/problems/add-two-numbers/description/ */
+
+import { ListNode } from './leetcode-2-add-two-numbers.types';
+
+export const addTwoNumbers = (
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null => {
+  let plus = 0;
+
+  let node: ListNode | null = null;
+  let next: ListNode | null = null;
+
+  while (true) {
+    if (l1 === null && l2 === null) {
+      if (next && plus > 0) {
+        next.next = new ListNode(plus);
+      }
+
+      break;
+    }
+
+    let val = plus;
+
+    if (l1) {
+      val += l1.val || 0;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      val += l2.val || 0;
+      l2 = l2.next;
+    }
+
+    if (val > 9) {
+      val -= 10;
+      plus = 1;
+    } else {
+      plus = 0;
+    }
+
+    if (node === null) {
+      node = new ListNode(val);
+      next = node;
+      continue;
+    }
+
+    if (next === null) {
+      node.next = new ListNode(val);
+      next = node.next;
+    } else {
+      next.next = new ListNode(val);
+      next = next.next;
+    }
+  }
+
+  return node;
+};
---
+++ typescript/test/leetcode-2-add-two-numbers.spec.ts
@@ -0,0 +1,72 @@
+import { ListNode } from 'typescript/solved/leetcode-2-add-two-numbers.types';
+import { addTwoNumbers } from 'typescript/solved/leetcode-2-add-two-numbers';
+
+const makeListNode = (values: number[]): ListNode | null => {
+  let node: null | ListNode = null;
+  let last: null | ListNode = null;
+
+  for (const value of values) {
+    if (node === null) {
+      node = new ListNode(value);
+      continue;
+    }
+
+    if (last === null) {
+      node.next = new ListNode(value);
+      last = node.next;
+    } else {
+      last.next = new ListNode(value);
+      last = last.next;
+    }
+  }
+
+  return node;
+};
+
+const getNodeValues = (node: ListNode | null) => {
+  const values: number[] = [];
+
+  while (node) {
+    values.push(node.val);
+    node = node.next;
+  }
+
+  return values;
+};
+
+describe('leetcode 02. Add Two Numbers', () => {
+  it('tc1', () => {
+    const l1 = makeListNode([2, 4, 9]);
+    const l2 = makeListNode([5, 6, 4, 9]);
+    const res = addTwoNumbers(l1, l2);
+    const values = getNodeValues(res);
+
+    expect(JSON.stringify(values)).toEqual(JSON.stringify([7, 0, 4, 0, 1]));
+  });
+
+  it('tc2', () => {
+    const l1 = makeListNode([
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 1,
+    ]);
+    const l2 = makeListNode([5, 6, 4]);
+    const res = addTwoNumbers(l1, l2);
+    const values = getNodeValues(res);
+
+    expect(JSON.stringify(values)).toEqual(
+      JSON.stringify([
+        6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1,
+      ]),
+    );
+  });
+
+  it('tc2', () => {
+    const l1 = makeListNode([5]);
+    const l2 = makeListNode([5]);
+    const res = addTwoNumbers(l1, l2);
+    const values = getNodeValues(res);
+
+    expect(JSON.stringify(values)).toEqual(JSON.stringify([0, 1]));
+  });
+});
