360ca27ecefba373e0f13ca5d8ba34ea58387e6e
choewy <choewy32@gmail.com>
2023-08-28 11:43:06 +0900

    feature/leetcode 1. two sum

---
+++ python/main.py
@@ -0,0 +1,5 @@
+from solved.leetcode_1_two_sum_2 import test
+
+
+if __name__ == "__main__":
+    test()
---
+++ python/solved/leetcode_1_two_sum_1.py
@@ -0,0 +1,39 @@
+'''
+description https://leetcode.com/problems/two-sum/description
+'''
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num1_cursor = -1
+        num2_cursor = 0
+
+        while True:
+            if num1_cursor > -1 and num2_cursor > 0:
+                break
+
+            num1 = nums.pop(0)
+
+            if num1 is None:
+                break
+
+            num1_cursor += 1
+
+            for i, num2 in enumerate(nums):
+                if (num1 + num2) == target:
+                    num2_cursor = num1_cursor + i + 1
+                    break
+
+        return [num1_cursor, num2_cursor]
+
+
+def test() -> None:
+    solution = Solution()
+
+    print(solution.twoSum([2, 7, 11, 15], 9) == [0, 1])
+    print(solution.twoSum([3, 2, 4], 6) == [1, 2])
+    print(solution.twoSum([3, 3], 6) == [0, 1])
+
+
+if __name__ == "__main__":
+    test()
