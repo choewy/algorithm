c029a6233a8c162f21f4c1773a49c326de8cf5d6
choewy <choewy32@gmail.com>
2023-08-28 12:03:07 +0900

    feature/main.py

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
-from solved.leetcode_1_two_sum_2 import test
+from solved import leetcode_1_two_sum_1, leetcode_1_two_sum_2
 
 
 if __name__ == "__main__":
-    test()
+    leetcode_1_two_sum_1.test()
+    leetcode_1_two_sum_2.test()
